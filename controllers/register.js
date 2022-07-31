f138cbcee0e7c4d06a342d848e05c72b8e89fb18
Duyen Nguyen <dnng1410@gmail.com>
2022-07-31 16:40:47 +0700

    add register.js and model User.js

---
+++ controllers/register.js
@@ -0,0 +1,12 @@
+// Phương thức POST: Thêm user
+document.querySelector("#btn-submit").onclick = () => {
+    // {
+    //     "email": "string",
+    //     "password": "string",
+    //     "name": "string",
+    //     "gender": true,
+    //     "phone": "string"
+    // }
+    // Lấy thông tin user đúng format back-end quy định
+    let 
+}
